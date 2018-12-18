36ebbef1e8dedcaeba9f4f52f827911d5e9967ab
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-12-18 16:57:57 -0500

    create core data models and relationships between persons and groups

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Johnny Perdomo on 12/18/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
